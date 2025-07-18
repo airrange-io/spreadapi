c7f2df55e7b4c776326284d8c3154073b227482e
airrange <72928857+airrange@users.noreply.github.com>
2025-07-18 17:49:39 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: '/favicon.ico', sizes: 'any' },
       { url: '/icons/icon-96x96.png', sizes: '96x96', type: 'image/png' },
       { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
